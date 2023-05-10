eb3f17c9a9f69eb85eb0867e9156f500dc72da7d
Eugene Walker <121591687+WalkerVision@users.noreply.github.com>
2023-05-10 10:44:21 -0400

    Update Program.cs
    Created player choice and choice validation  with while loop

--- rock paper scissors/Program.cs
+++ rock paper scissors/Program.cs
@@ -1,3 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!dcddvv");
+﻿using System;
 
+Console.WriteLine("Rock,Paper,Scissors Enter your choice!");
+string playerchoice = Console.ReadLine();
+
+while (playerchoice != "Rock" && playerchoice != "Paper" && playerchoice != "Scissors")
+{
+    Console.WriteLine("Invalid choice please enter Rock,Paper,Scissors!");
+    playerchoice = Console.ReadLine();
+}
