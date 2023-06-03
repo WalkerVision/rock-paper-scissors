614422ee8fddf4f4af5d2321aca3e74f73a53344
Eugene Walker <121591687+WalkerVision@users.noreply.github.com>
2023-06-02 22:04:33 -0400

    Update Program.cs
    added while loop to continue the game

--- rock paper scissors/Program.cs
+++ rock paper scissors/Program.cs
@@ -2,55 +2,70 @@
 
 class Program
 {
-    static string[] choices = { "Rock", "Paper", "Scissors" };
+
     static void Main(string[] args)
     {
-
-        Console.WriteLine("Rock,Paper,Scissors Enter your choice!");
-        string playerchoice = Console.ReadLine();
-
-        // added a chioce validation with a while loop
-        while (playerchoice != "Rock" && playerchoice != "Paper" && playerchoice != "Scissors")
+        while (true)
         {
-            Console.WriteLine("Invalid choice please enter Rock,Paper,Scissors!");
-            playerchoice = Console.ReadLine();
-        }
-        //while (true)
-        //generate computer's choice
-        Random random = new Random();
-        string[] choices = { "Rock", "Paper", "Scissors" };
+            Console.WriteLine("Rock,Paper,Scissors Enter your choice!");
+            string userChoice = Console.ReadLine();
 
-        int computerChoiceIndex = random.Next(0, 3);
-        string computerChoice = choices[computerChoiceIndex];
+            // added a chioce validation with a while loop
+            while (userChoice != "Rock" && userChoice != "Paper" && userChoice != "Scissors")
+            {
+                Console.WriteLine("Invalid choice please enter Rock,Paper,Scissors!");
+                userChoice = Console.ReadLine();
+            }
 
-        string userChoice = playerchoice;
-        string result;
+            //generate computer's choice
+            Random random = new Random();
+            string[] choices = { "Rock", "Paper", "Scissors" };
+            int computerChoiceIndex = random.Next(0, 3);
+            string computerChoice = choices[computerChoiceIndex];
 
-        if (userChoice == computerChoice)
-        {
-            result = "It's a tie!";
-        }
-        else if ((userChoice == "Rock" && computerChoice == "Scissors") || (userChoice == "Paper" && computerChoice == "Rock") || (userChoice == "Scissors" && computerChoice == "Paper"))
-        {
-            result = "You Win";
-        }
-        else
-        {
-            result = "Computer wins!";
-        }
+            //string userChoice = playerchoice;
+            string result;
 
-        // display result
-        Console.WriteLine($"your choice: {userChoice}");
-        Console.WriteLine($"computer's choice: {computerChoice}");
-        Console.WriteLine(result);
+            if (userChoice == computerChoice)
+            {
+                result = "It's a tie!";
+            }
+            else if ((userChoice == "Rock" && computerChoice == "Scissors") || (userChoice == "Paper" && computerChoice == "Rock") || (userChoice == "Scissors" && computerChoice == "Paper"))
+            {
+                result = "You Win";
+            }
+            else
+            {
+                result = "Computer wins!";
+            }
 
-        // wait for user input to exit
-        Console.WriteLine("Press agin key to exit...");
-        Console.ReadLine();
+            // display result
+            Console.WriteLine($"your choice: {userChoice}");
+            Console.WriteLine($"computer's choice: {computerChoice}");
+            Console.WriteLine(result);
+
+            //ask player if they want to play again
+            Console.WriteLine("Would you like to play again? Y/N ");
+            string playAgainInput = Console.ReadLine();
+
+            if (playAgainInput.ToUpper() != "Y")
+            {
+                break;
+            }
 
 
+            Console.WriteLine("Thanks for playing");
+
+
+
+        }   
+            // wait for user input to exit
+            Console.WriteLine("Press agin key to exit...");
+            Console.ReadLine();
+
+
+
+        
 
     }
-
 }
-
